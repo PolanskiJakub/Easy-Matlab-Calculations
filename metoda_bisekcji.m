f0a62ca0a54627ad52486db00fb3d8672e0e16ec
Eryk <eryktkaczewski@gmail.com>
2022-04-04 17:47:18 +0200

    Update metoda_bisekcji.m

--- metoda_bisekcji.m
+++ metoda_bisekcji.m
@@ -7,7 +7,7 @@ b=4;
 
 x1=(a+b)/2;
 Tol=10^-5;
-y=funct2(x1);
+y=funct(x1);
 if(y==0)
     return;
 end
@@ -15,18 +15,18 @@ end
 x1=b;
 while(abs(a-b)>Tol)
     x1=(a+b)/2;
-        if(funct2(a)*funct2(x1)<0)
+        if(funct(a)*funct(x1)<0)
             b=x1;
-        elseif(funct2(x1)*funct2(b)<0)
+        elseif(funct(x1)*funct(b)<0)
             a=x1;
         end
 end
 
     function y = funct(x)
-           y=x+log(x)-sin(x)^2;
+           y=x+log10(x)-sin(x)^2;
     end
     
     function y = funct2(x)
-           y=x^2+log(x)-sin(x);
+           y=x^2+log10(x)-sin(x);
     end
 
